9f7f34c855c44785c758cea71ed322186d1fdc78
bhammermann <bjoern.hammermann03@gmail.com>
2024-02-06 11:16:02 +0100

    implemented loop that executes 1 function every 10 minutes

--- scraping/main.py
+++ scraping/main.py
@@ -1,9 +1,9 @@
 # main_file.py
+import time
 from scraping_functions import GPU_Prices, CPU_Prices, Main_Prices, PSU_Prices, RAM_Prices, Case_Prices
 
-GPU_Prices()
-CPU_Prices()
-Main_Prices()
-PSU_Prices()
-RAM_Prices()
-Case_Prices()
+functions = [GPU_Prices, CPU_Prices, Main_Prices, PSU_Prices, RAM_Prices, Case_Prices]
+
+for func in functions:
+    func()
+    time.sleep(600)
