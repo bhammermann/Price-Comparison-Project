aa3ff3ef07926a25a75aa1240f2685116d3e09a4
bhammermann <bjoern.hammermann03@gmail.com>
2024-02-09 13:59:07 +0100

    Revert "tried fixing structure & seperate frontend/backend"
    This reverts commit 9451b5c1c11e73318ddee42b99c8cc15ceb550c5.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
 require('dotenv').config()
 const app = require('./app');
 
